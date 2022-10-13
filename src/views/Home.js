0397b33e8f2d162735d50886b4e00a816e636639
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-10-13 13:28:42 +0000

    Updates

---
+++ src/views/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import sections
+import Hero from '../components/sections/Hero';
+import FeaturesTiles from '../components/sections/FeaturesTiles';
+
+const Home = () => {
+
+  return (
+    <>
+      <Hero />
+      <FeaturesTiles topDivider />
+    </>
+  );
+}
+
+export default Home;
