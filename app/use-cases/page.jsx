66c239a943157d5a483e5da8a9f1ef373e70d411
Andrew Lahiff <andrew.lahiff@gmail.com>
2024-04-15 16:58:14 +0100

    Add draft of use cases

---
+++ app/use-cases/page.jsx
@@ -0,0 +1,184 @@
+export default function Page() {
+  return (
+    <div className="bg-white dark:bg-slate-900">
+      <header className="sticky top-0 inset-x-0 flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-slate-900 dark:border-gray-700">
+        <nav
+          className="relative w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex items-center justify-between">
+            <a
+              className="flex-none text-xl font-semibold dark:text-white"
+              href="/"
+              aria-label="Simvue logo"
+            >
+              <img
+                className="color-invertible"
+                src="simvue-black-small.png"
+                width="120"
+                height="28"
+                alt="Simvue Logo"
+              />
+            </a>
+            <div className="sm:hidden">
+              <button
+                type="button"
+                className="hs-collapse-toggle size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-gray-700 dark:hover:bg-gray-700"
+                data-hs-collapse="#navbar-collapse-with-animation"
+                aria-controls="navbar-collapse-with-animation"
+                aria-label="Toggle navigation"
+              >
+                <svg
+                  className="hs-collapse-open:hidden flex-shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <line x1="3" x2="21" y1="6" y2="6" />
+                  <line x1="3" x2="21" y1="12" y2="12" />
+                  <line x1="3" x2="21" y1="18" y2="18" />
+                </svg>
+                <svg
+                  className="hs-collapse-open:block hidden flex-shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M18 6 6 18" />
+                  <path d="m6 6 12 12" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div
+            id="navbar-collapse-with-animation"
+            className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
+          >
+            <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:ps-7">
+              <a
+                className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+                href="/"
+              >
+                Home
+              </a>
+              <a
+                className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+                href="https://docs.simvue.io"
+              >
+                Docs
+              </a>
+              <a
+                className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+                href="/use-cases"
+              >
+                Use cases
+              </a>
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      <main id="content">
+        <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+          <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+            <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
+              Use cases
+            </h2>
+            <p class="mt-1 text-gray-600 dark:text-gray-400">
+              A selection of examples illustrating just some of the features of
+              Simvue
+            </p>
+          </div>
+
+          <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            <a class="group" href="/use-cases/openfoam">
+              <div class="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
+                <img
+                  class="size-full absolute top-0 start-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl"
+                  src="https://s3-eu-west-1.amazonaws.com/fetchcfd/original/file-1502396856754.jpg"
+                  alt="Image Description"
+                />
+              </div>
+
+              <div class="mt-7">
+                <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
+                  OpenFOAM
+                </h3>
+                <p class="mt-3 text-gray-800 dark:text-gray-200">
+                  Automatically monitor the convergence of OpenFOAM CFD
+                  simulations.
+                </p>
+                <p class="mt-5 inline-flex items-center gap-x-1 text-blue-600 decoration-2 group-hover:underline font-medium">
+                  Read more
+                  <svg
+                    class="flex-shrink-0 size-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="m9 18 6-6-6-6" />
+                  </svg>
+                </p>
+              </div>
+            </a>
+
+            <a class="group" href="#">
+              <div class="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
+                <img
+                  class="size-full absolute top-0 start-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl"
+                  src="https://media.datacenterdynamics.com/media/images/tileflow-CFD.original.jpg"
+                  alt="Image Description"
+                />
+              </div>
+
+              <div class="mt-7">
+                <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
+                  FDS
+                </h3>
+                <p class="mt-3 text-gray-800 dark:text-gray-200">
+                  Reduce computing costs by ensuring only those simulation
+                  meeting specified requirements run to completion.
+                </p>
+                <p class="mt-5 inline-flex items-center gap-x-1 text-blue-600 decoration-2 group-hover:underline font-medium">
+                  Read more
+                  <svg
+                    class="flex-shrink-0 size-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="m9 18 6-6-6-6" />
+                  </svg>
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
