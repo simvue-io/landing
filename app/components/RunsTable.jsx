9b873e1aa95809883ac5850219f2c97ae96b3123
Andrew Lahiff <andrew.lahiff@gmail.com>
2024-05-08 19:30:10 +0100

    Tidying

--- app/components/RunsTable.jsx
+++ app/components/RunsTable.jsx
@@ -5,6 +5,7 @@ function spark(data) {
       <SparkLineChart
         data={data}
         height={34}
+        width={150}
         area
         showTooltip
         showHighlight
@@ -42,7 +43,7 @@ export default function Footer() {
                 <th scope="col" className="px-6 py-3 text-start">
                   <div className="flex items-center gap-x-2">
                     <span className="text-xs font-semibold tracking-wide text-gray-800 dark:text-neutral-200">
-                      Water droplet diameter
+                      Droplet diameter
                     </span>
                   </div>
                 </th>
@@ -145,11 +146,7 @@ export default function Footer() {
                   </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
-                  <button type="button" className="block" data-hs-overlay="#hs-ai-invoice-modal">
-                    <span className="block px-6 py-2">
                       <span className="text-xs text-gray-600 dark:text-neutral-400">{ spark([0.0, 0.0, 3.36101055, 15.666974, 24.28424366666667, 36.207769, 55.720214, 164.7559985, 1063.1372133333332, 2090.6468, 2853.95735, 2908.1389, 2925.858, 2132.1486, 2258.8241, 2739.0973000000004, 2229.5129333333334, 1234.54683, 243.86188, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]) } </span>
-                    </span>
-                  </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
                 </td>
@@ -190,11 +187,7 @@ export default function Footer() {
                   </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
-                  <button type="button" className="block" data-hs-overlay="#hs-ai-invoice-modal">
-                    <span className="block px-6 py-2">
                       <span className="text-xs text-gray-600 dark:text-neutral-400"> { spark([0.0, 0.0, 3.36101055, 15.666974, 24.28424366666667, 36.207769, 55.720214, 164.7559985, 1063.1372133333332, 2090.6468, 2853.95735, 2917.4199, 2360.616433333333, 1844.6898500000002, 1762.3378, 1778.5886, 1234.4597999999999, 1494.7573, 467.12597500000004, 283.24345500000004, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]) }</span>
-                    </span>
-                  </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
                 </td>
@@ -235,11 +228,7 @@ export default function Footer() {
                   </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
-                  <button type="button" className="block" data-hs-overlay="#hs-ai-invoice-modal">
-                    <span className="block px-6 py-2">
                       <span className="text-xs text-gray-600 dark:text-neutral-400"> { spark([0.0, 0.0, 3.36101055, 15.666974, 24.28424366666667, 36.207769, 55.720214, 164.7559985, 1063.1372133333332, 2090.6468, 2853.95735, 2917.4671, 2344.8263666666667, 1917.96975, 1965.4135999999999, 1484.6258, 1416.0669333333335, 1656.2979, 1115.45525, 911.127205, 1181.0654233333332, 1401.18585, 1187.2669, 834.10749, 813.6964766666666, 959.0791650000001, 863.29874, 885.139055, 848.54385, 756.9149150000001, 7.9717195, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]) }</span>
-                    </span>
-                  </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
                 </td>
@@ -280,11 +269,7 @@ export default function Footer() {
                   </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
-                  <button type="button" className="block" data-hs-overlay="#hs-ai-invoice-modal">
-                    <span className="block px-6 py-2">
                       <span className="text-xs text-gray-600 dark:text-neutral-400"> { spark([0.0, 0.0, 3.36101055, 15.666974, 24.28424366666667, 36.207769, 55.720214, 164.7559985, 1063.1372133333332, 2090.6468, 2853.95735, 2932.0959000000003, 1923.6998333333331, 1208.5475999999999, 1174.6804499999998, 1348.7685999999999, 1382.6782666666668, 615.19264, 0.0076650388, 0.0030874187200000004, 0.000186512701, 2.585332e-05, 5.36156445e-06, 1.034022795e-06, 1.4255597333333333e-07, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]) }</span>
-                    </span>
-                  </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
                 </td>
@@ -325,11 +310,7 @@ export default function Footer() {
                   </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
-                  <button type="button" className="block" data-hs-overlay="#hs-ai-invoice-modal">
-                    <span className="block px-6 py-2">
                       <span className="text-xs text-gray-600 dark:text-neutral-400"> { spark([0.0, 0.0, 3.36101055, 15.666974, 24.28424366666667, 36.207769, 55.720214, 164.7559985, 1063.1372133333332, 2090.6468, 2853.95735, 2908.1389, 2925.858, 2132.1486, 2258.8241, 2741.4105, 2224.487266666667, 1240.58549, 1396.3406, 1202.4917, 1101.7391333333333, 2233.2129, 1049.55312, 1069.9042299999999, 804.0724799999999, 844.5637899999999, 348.814405, 203.04355999999999, 3.178797466666667, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]) } </span>
-                    </span>
-                  </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
                 </td>
@@ -370,11 +351,7 @@ export default function Footer() {
                   </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
-                  <button type="button" className="block" data-hs-overlay="#hs-ai-invoice-modal">
-                    <span className="block px-6 py-2">
                       <span className="text-xs text-gray-600 dark:text-neutral-400"> { spark([0.0, 0.0, 3.36101055, 15.666974, 24.28424366666667, 36.207769, 55.720214, 164.7559985, 1063.1372133333332, 2090.6468, 2853.95735, 2939.1164, 1974.653833333333, 1066.36087, 1148.43697, 1184.6770000000001, 1124.0234833333334, 814.895355, 39.222372935, 0.000790124105, 5.577238199999999e-05, 9.067211764999999e-07, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]) }</span>
-                    </span>
-                  </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
                 </td>
@@ -415,11 +392,7 @@ export default function Footer() {
                   </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
-                  <button type="button" className="block" data-hs-overlay="#hs-ai-invoice-modal">
-                    <span className="block px-6 py-2">
                       <span className="text-xs text-gray-600 dark:text-neutral-400"> { spark([0.0, 0.0, 3.36101055, 15.666974, 24.28424366666667, 36.207769, 55.720214, 164.7559985, 1063.1372133333332, 2090.6468, 2853.95735, 2918.3836499999998, 1995.7168333333332, 1364.5574, 1087.63305, 1383.4258, 1570.0206333333333, 774.12559, 16.64277476605, 0.0012632414000000002, 0.00013888764966666665, 6.01844955e-07, 1.0789259e-08, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]) } </span>
-                    </span>
-                  </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
                 </td>
@@ -460,11 +433,7 @@ export default function Footer() {
                   </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
-                  <button type="button" className="block" data-hs-overlay="#hs-ai-invoice-modal">
-                    <span className="block px-6 py-2">
                       <span className="text-xs text-gray-600 dark:text-neutral-400"> { spark([0.0, 0.0, 3.36101055, 15.666974, 24.28424366666667, 36.207769, 55.720214, 164.7559985, 1063.1372133333332, 2090.6468, 2853.95735, 2908.1389, 2925.858, 2132.1486, 2258.8241, 2735.45935, 2006.7359000000004, 1943.05805, 2217.9428500000004, 1338.36605, 2314.6182999999996, 2087.70275, 1992.3782, 1947.7808, 683.8574533333334, 1049.3865700000001, 1150.9697, 1335.77545, 919.05956, 712.789225, 804.385625, 485.91743, 261.180331, 111.74024, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]) }</span>
-                    </span>
-                  </button>
                 </td>
                 <td className="size-px whitespace-nowrap">
                 </td>
