a8d019b74011ca082a5e0217acf0d0936f24ac31
Andrew Lahiff <andrew.lahiff@gmail.com>
2024-05-08 19:30:01 +0100

    Fix packages

--- app/page.jsx
+++ app/page.jsx
@@ -339,7 +339,7 @@ green computing.
           </div>
         </div>
 
-        <div className="max-w-5xl mx-auto text-center pt-20 pb-1 lg:pb-1">
+        <div className="max-w-4xl mx-auto text-center pt-20 pb-1 lg:pb-1">
           <h2 className="text-2xl font-bold sm:text-3xl md:text-4xl dark:text-white">
             Why Simvue?
           </h2>
@@ -355,50 +355,50 @@ green computing.
 </div>
 
 
-<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-  <div class="md:grid md:grid-cols-3 md:items-center md:gap-12 xl:gap-32">
-    <div class="col-span-2">
+<div className="max-w-[85rem] px-4 py-4 sm:px-6 lg:px-8 lg:py-8 mx-auto">
+  <div className="md:grid md:grid-cols-3 md:items-center md:gap-12 xl:gap-32">
+    <div className="col-span-2">
       <RunsTable />
     </div>
 
-    <div class="mt-5 sm:mt-10 lg:mt-0">
-      <div class="space-y-6 sm:space-y-8">
-        <div class="space-y-2 md:space-y-4">
-          <h2 class="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-neutral-200">
-            Tracking
+    <div className="mt-5 sm:mt-5 lg:mt-0">
+      <div className="space-y-6 sm:space-y-8">
+        <div className="space-y-2 md:space-y-4">
+          <h2 className="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-neutral-200">
+            Tracking & monitoring
           </h2>
-          <p class="text-gray-500 dark:text-neutral-500">
+          <p className="text-gray-500 dark:text-neutral-500">
             Organise and track any number of any application running anywhere.
           </p>
         </div>
 
-        <ul class="space-y-2 sm:space-y-4">
-          <li class="flex space-x-3">
-            <span class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
-              <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+        <ul className="space-y-2 sm:space-y-4">
+          <li className="flex space-x-3">
+            <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+              <svg className="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
             </span>
 
-            <span class="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+            <span className="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
               Search, sort and filter
             </span>
           </li>
 
-          <li class="flex space-x-3">
-            <span class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
-              <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <li className="flex space-x-3">
+            <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+              <svg className="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
             </span>
 
-            <span class="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+            <span className="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
               Compare
             </span>
           </li>
 
-          <li class="flex space-x-3">
-            <span class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
-              <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <li className="flex space-x-3">
+            <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+              <svg className="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
             </span>
 
-           <span class="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+           <span className="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
              Live monitoring
             </span>
           </li>
