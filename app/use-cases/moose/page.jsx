9548593fa5485d3c803c086604f649cd3d366d46
Andrew Lahiff <andrew.lahiff@gmail.com>
2024-05-30 15:57:39 +0100

    Reduce font size of title

--- app/use-cases/moose/page.jsx
+++ app/use-cases/moose/page.jsx
@@ -9,7 +9,7 @@ export default function UseCase() {
         <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-12 pb-12">
           <div className="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
             <div>
-              <h2 className="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white">
+              <h2 className="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-5xl lg:leading-tight dark:text-white">
                 Monitor your <span className="text-blue-600">MOOSE</span>{" "}
                 applications in real time
               </h2>
