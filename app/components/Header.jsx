b53fd2325ea7eae3332b8b104856707642c4a095
Andrew Lahiff <andrew.lahiff@gmail.com>
2024-05-30 14:38:37 +0100

    Update header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,5 @@
 export default function Header() {
   return (
-    <div className="bg-white dark:bg-slate-900">
       <header className="sticky top-0 inset-x-0 flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-slate-900 dark:border-gray-700">
         <nav
           className="relative w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
@@ -14,7 +13,7 @@ export default function Header() {
             >
               <img
                 className="color-invertible"
-                src="simvue-black-small.png"
+                src="/simvue-black-small.png"
                 width="120"
                 height="28"
                 alt="Simvue Logo"
@@ -36,9 +35,9 @@ export default function Header() {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <line x1="3" x2="21" y1="6" y2="6" />
                   <line x1="3" x2="21" y1="12" y2="12" />
@@ -52,9 +51,9 @@ export default function Header() {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path d="M18 6 6 18" />
                   <path d="m6 6 12 12" />
@@ -69,7 +68,7 @@ export default function Header() {
             <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:ps-7">
               <a
                 className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
-                href="#"
+                href="/landing"
               >
                 Home
               </a>
@@ -77,18 +76,29 @@ export default function Header() {
                 className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
                 href="https://docs.simvue.io"
               >
-                Docs
+                Documentation
               </a>
-              <a
-                className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
-                href="/use-cases"
-              >
+            <div className="hs-dropdown [--strategy:static] md:[--strategy:absolute] [--adaptive:none] md:[--trigger:hover] py-3 md:px-3 md:py-6">
+              <button type="button" className="flex items-center w-full text-gray-500 hover:text-gray-400 font-medium dark:text-gray-400 dark:hover:text-gray-500">
                 Use cases
-              </a>
+                <svg className="flex-shrink-0 ms-2 size-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path>
+                </svg>
+              </button>
+
+              <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] md:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 md:w-80 hidden z-10 bg-white md:shadow-2xl rounded-lg py-2 md:p-2 dark:bg-neutral-800 dark:divide-neutral-700 before:absolute right-0 top-full before:-top-5 before:start-0 before:w-full before:h-5">
+                <a className="inline-flex gap-x-5 w-full p-4 text-gray-600 rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300" href="/use-cases/moose">
+                  <div className="grow">
+                    <span className="block font-semibold mb-1 text-gray-800 dark:text-neutral-200">MOOSE</span>
+Monitor your MOOSE applications in real time
+                  </div>
+                </a>
+
+              </div>
+            </div>
             </div>
           </div>
         </nav>
       </header>
-    </div>
   );
 }
