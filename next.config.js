ae32ef567f7fa472962c7ca9e4e59a0e593e2f4d
Andrew Lahiff <andrew.lahiff@gmail.com>
2024-06-07 11:28:53 +0100

    Reverted

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /**
  * @type {import('next').NextConfig}
  */
+const nextConfig = {
+  output: 'export',
 
-const { withPlausibleProxy } = require('next-plausible')
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
 
-module.exports = withPlausibleProxy()({});
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+module.exports = nextConfig
