b8c01300d80e4d5749103f060e1a87eb0e63e2a7
Andrew Lahiff <andrew.lahiff@gmail.com>
2024-06-07 11:24:17 +0100

    Simplify

--- next.config.js
+++ next.config.js
@@ -2,18 +2,6 @@
  * @type {import('next').NextConfig}
  */
 
-const nextConfig = {
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
 const { withPlausibleProxy } = require('next-plausible')
 
-module.exports = withPlausibleProxy()(nextConfig);
+module.exports = withPlausibleProxy()({});
