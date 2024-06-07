80c1a17a014376ff2af8455be4a7e8e672c56199
Andrew Lahiff <andrew.lahiff@gmail.com>
2024-06-07 15:29:54 +0100

    Add UKAEA logo

--- app/page.jsx
+++ app/page.jsx
@@ -9,45 +9,51 @@ export default function Home() {
 
   return (
     <div className="bg-white dark:bg-slate-900">
-      <HeaderLogoOnly />
+      <div className="flex flex-col h-screen justify-between">
+        <main id="content" className="mb-auto">
+          <HeaderLogoOnly />
 
-      <main id="content">
+          <div className="max-w-[85rem] mx-auto pt-32 pb-4 px-4 sm:px-6 lg:px-8 md:pt-16 md:pb-8">
+            <div className="relative overflow-hidden">
+              <div className="mt-5 max-w-2xl text-center mx-auto">
+                <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
+                  Simulation{" "}
+                  <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
+                    tracking
+                  </span>{" "}
+                  and{" "}
+                  <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
+                    monitoring
+                  </span>
+                </h1>
+              </div>
 
-        <div className="max-w-[85rem] mx-auto pt-32 pb-4 px-4 sm:px-6 lg:px-8 md:pt-16 md:pb-8">
-          <div className="relative overflow-hidden">
-            <div className="mt-5 max-w-2xl text-center mx-auto">
-              <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
-                Simulation{" "}
-                <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
-                  tracking
-                </span>{" "}
-                and{" "}
-                <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
-                  monitoring
-                </span>
-              </h1>
-            </div>
+              <div className="mt-5 max-w-3xl text-center mx-auto">
+                <p className="text-lg text-gray-600 dark:text-gray-400">
+                  Organise and gain insights into simulations, data processing
+                  or AI/ML training by capturing metadata and data combined with
+                  real-time monitoring, logging and alerting.
+                </p>
+              </div>
 
-            <div className="mt-5 max-w-3xl text-center mx-auto">
-              <p className="text-lg text-gray-600 dark:text-gray-400">
-                Organise and gain insights into simulations, data processing or AI/ML training by
-                capturing metadata and data combined with real-time monitoring,
-                logging and alerting.
-              </p>
-            </div>
-
-            <div className="mt-8 gap-3 flex justify-center">
-              <button
-                type="button"
-                className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full bg-gradient-to-tl from-blue-600 to-blue-400 hover:from-blue-400 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800 disabled:pointer-events-none"
-                onClick={() => router.push("mailto:info@simvue.io")}
-              >
-                Contact us for more information
-              </button>
+              <div className="mt-8 gap-3 flex justify-center">
+                <button
+                  type="button"
+                  className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full bg-gradient-to-tl from-blue-600 to-blue-400 hover:from-blue-400 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800 disabled:pointer-events-none"
+                  onClick={() => router.push("mailto:info@simvue.io")}
+                >
+                  Contact us for more information
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
+        </main>
+        <footer className="ml-auto w-full py-6 px-4 sm:px-6 lg:px-8 mx-auto dark:bg-slate-900">
+          <div className="flex justify-end">
+            <img src="/UKAEA.png" width="213" height="24" alt="UKAEA" />
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
