4d8701e9ebe6861bfcffe5635932f4b4a791b74f
AbyAbraham21 <95077500+AbyAbraham21@users.noreply.github.com>
2024-09-17 11:51:20 +0100

    Add sustainability (#1)
    * Update page.jsx
    
    Added mention to sustainability
    
    * Update page.jsx
    
    adding reference to sustainability
    
    * Update page.jsx
    
    reworded
    
    * Update page.jsx
    
    reword
    
    * Update page.jsx
    
    reword
    
    * Update page.jsx
    
    simplifying text
    
    * Update page.jsx
    
    reworded
    
    * Increase width & correct font colour
    
    * Increase width & correct font colour
    
    * Update page.jsx
    
    reworded
    
    * Update page.jsx
    
    reworded to flow better
    
    * Replace blue/purple with green
    
    * Replace blue/purple with green
    
    * Change to green version
    
    * Adjust colours on hover
    
    * Change to green
    
    ---------
    
    Co-authored-by: alahiff <alahiff@users.noreply.github.com>
    Co-authored-by: Andrew Lahiff <andrew.lahiff@gmail.com>

--- app/innovation-2024/page.jsx
+++ app/innovation-2024/page.jsx
@@ -26,30 +26,26 @@ export default function Home() {
 
         <div className="max-w-[85rem] mx-auto pt-4 pb-4 px-4 sm:px-6 lg:px-8 md:pt-8 md:pb-8">
           <div className="relative overflow-hidden">
-            <div className="mt-5 max-w-2xl text-center mx-auto">
+            <div className="mt-5 max-w-3xl text-center mx-auto">
               <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
-                Simulation{" "}
-                <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
-                  tracking
+                Unlocking{" "}
+                <span className="bg-clip-text bg-gradient-to-tl from-green-600 to-lime-600 text-transparent">
+                  sustainability
                 </span>{" "}
-                and{" "}
-                <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
-                  monitoring
-                </span>
+                for any simulation, data processing or AI/ML tasks
               </h1>
             </div>
 
             <div className="mt-5 max-w-3xl text-center mx-auto">
               <p className="text-lg text-gray-600 dark:text-gray-400">
-                Organise and gain insights into simulations, data processing or AI/ML training by
-                capturing metadata and data combined with real-time monitoring,
-                logging and alerting.
+                Innovative solutions for unlocking sustainability at an environmental, social and economic scale by capturing and tracking metadata, using real-time monitoring,   
+                alerting and logging to organise and gain insights.
               </p>
             </div>
 
             <div className="mt-8 gap-3 flex justify-center">
               <a
-                className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-blue-400 hover:from-blue-400 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800"
+                className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-green-600 to-lime-600 hover:from-lime-500 hover:to-green-500 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800"
                 href="https://docs.simvue.io/introduction/"
               >
                 Get started
@@ -455,7 +451,7 @@ green computing.
 
 
         <div className="pt-6 pb-2">
-          <div className="w-full mx-auto pt-16 pb-16 text-center bg-gradient-to-br from-violet-700 to-sky-500">
+          <div className="w-full mx-auto pt-16 pb-16 text-center bg-gradient-to-br from-teal-700 to-green-500">
             <h2 className="text-2xl font-bold sm:text-3xl md:text-4xl text-white">
               Are you ready to take control?
             </h2>
--- app/page.jsx
+++ app/page.jsx
@@ -15,31 +15,28 @@ export default function Home() {
 
           <div className="max-w-[85rem] mx-auto pt-32 pb-4 px-4 sm:px-6 lg:px-8 md:pt-16 md:pb-8">
             <div className="relative overflow-hidden">
-              <div className="mt-5 max-w-2xl text-center mx-auto">
+              <div className="mt-5 max-w-3xl text-center mx-auto">
                 <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
-                  Simulation{" "}
-                  <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
-                    tracking
+                  Unlocking {" "}
+                  <span className="bg-clip-text bg-gradient-to-tl from-green-600 to-lime-600 text-transparent">
+                    sustainability
                   </span>{" "}
-                  and{" "}
-                  <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
-                    monitoring
-                  </span>
+                  for {" "}
+                    any simulation, data processing or AI/ML tasks
                 </h1>
               </div>
 
               <div className="mt-5 max-w-3xl text-center mx-auto">
                 <p className="text-lg text-gray-600 dark:text-gray-400">
-                  Organise and gain insights into simulations, data processing
-                  or AI/ML training by capturing metadata and data combined with
-                  real-time monitoring, logging and alerting.
+                Innovative solutions for unlocking sustainability at an environmental, social and economic scale by capturing and tracking metadata, using real-time monitoring,   
+                alerting and logging to organise and gain insights.
                 </p>
               </div>
 
               <div className="mt-8 gap-3 flex justify-center">
                 <button
                   type="button"
-                  className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full bg-gradient-to-tl from-blue-600 to-blue-400 hover:from-blue-400 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800 disabled:pointer-events-none"
+                  className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full bg-gradient-to-tl from-green-600 to-lime-600 hover:from-lime-500 hover:to-green-500 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800 disabled:pointer-events-none"
                   onClick={() => router.push("mailto:info@simvue.io")}
                 >
                   Contact us for more information
