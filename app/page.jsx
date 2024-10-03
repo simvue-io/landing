f820bd3031f52006ad6fa410e6d77ec930a4c914
Andrew Lahiff <andrew.lahiff@gmail.com>
2024-10-03 13:11:41 +0100

    Add new YouTube video to landing page

--- app/page.jsx
+++ app/page.jsx
@@ -13,27 +13,28 @@ export default function Home() {
         <main id="content" className="mb-auto">
           <HeaderLogoOnly />
 
-          <div className="max-w-[85rem] mx-auto pt-32 pb-4 px-4 sm:px-6 lg:px-8 md:pt-16 md:pb-8">
+          <div className="max-w-[85rem] mx-auto pt-8 pb-4 px-4 sm:px-6 lg:px-8 md:pt-8 md:pb-8">
             <div className="relative overflow-hidden">
               <div className="mt-5 max-w-3xl text-center mx-auto">
                 <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
-                  Unlocking {" "}
+                  Unlocking{" "}
                   <span className="bg-clip-text bg-gradient-to-tl from-green-600 to-lime-600 text-transparent">
                     sustainability
                   </span>{" "}
-                  for {" "}
-                    any simulation, data processing or AI/ML tasks
+                  for any simulation, data processing or AI/ML tasks
                 </h1>
               </div>
 
-              <div className="mt-5 max-w-3xl text-center mx-auto">
+              <div className="mt-5 max-w-5xl text-center mx-auto">
                 <p className="text-lg text-gray-600 dark:text-gray-400">
-                Innovative solutions for unlocking sustainability at an environmental, social and economic scale by capturing and tracking metadata, using real-time monitoring,   
-                alerting and logging to organise and gain insights.
+                  Innovative solutions for unlocking sustainability at an
+                  environmental, social and economic scale by capturing and
+                  tracking metadata, using real-time monitoring, alerting and
+                  logging to organise and gain insights.
                 </p>
               </div>
 
-              <div className="mt-8 gap-3 flex justify-center">
+              <div className="mt-6 gap-3 flex justify-center">
                 <button
                   type="button"
                   className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full bg-gradient-to-tl from-green-600 to-lime-600 hover:from-lime-500 hover:to-green-500 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800 disabled:pointer-events-none"
@@ -44,10 +45,33 @@ export default function Home() {
               </div>
             </div>
           </div>
+
+          <div className="max-w-[85rem] mx-auto pt-2 pb-8 px-4 sm:px-6 lg:px-8 md:pt-8 md:pb-8">
+            <div className="flex justify-center">
+              <iframe
+                className="max-w-xl aspect-video rounded-lg"
+                src="https://www.youtube.com/embed/6NCAYzay-A8"
+                width="100%"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
         </main>
-        <footer className="ml-auto w-full py-6 px-4 sm:px-6 lg:px-8 mx-auto dark:bg-slate-900">
-          <div className="flex justify-end">
-            <img className="color-invertible" src="/UKAEA.png" width="213" height="24" alt="UKAEA" />
+        <footer className="ml-auto w-full py-4 px-4 sm:px-4 lg:px-4 mx-auto dark:bg-slate-900">
+          <div className="flex justify-between">
+            <div className="mt-2 text-xs sm:text-sm text-gray-600 dark:text-neutral-400">
+              Simvue is being developed at the UK Atomic Energy Authority
+            </div>
+            <img
+              className="color-invertible"
+              src="/UKAEA.png"
+              width="256"
+              height="28"
+              alt="UKAEA"
+            />
           </div>
         </footer>
       </div>
